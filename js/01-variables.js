2d4a6cc22c61a23ce71e2923aca1875afbed6c6f
Auguste <abagdzeviciute@gmail.com>
2021-03-10 16:18:35 +0200

    js intro

---
+++ js/01-variables.js
@@ -0,0 +1,82 @@
+console.log(346);
+console.log("Vakaras");
+console.log(3, "rytas");
+console.log("Labas", 456, "rytas");
+console.log(57, "Labas", 65);
+console.log("Labas rytas, Lietuva");
+console.log(4.56);
+
+console.log("------------");
+
+// KINTAMUJU INICIAVIMO BUDAI
+ 
+
+let vardas = "Auguste";
+console.log(vardas);
+
+vardas = "Maryte";
+console.log(vardas);
+
+const pasisveikinimas = "Labas rytas, Lietuva!";
+console.log(pasisveikinimas);
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 8;
+const suma = pirmasSkaicius + antrasSkaicius;
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+console.log(suma);
+
+console.log(pirmasSkaicius, "+", antrasSkaicius, "=", suma);
+
+const mathText = pirmasSkaicius + " + " + antrasSkaicius + " = " + suma;
+console.log(mathText);
+
+console.log("------------");
+
+const petrasname = "Petras";
+const age = 45;
+const petrasage = petrasname + " ahe is " + age + ".";
+console.log(petrasage);
+
+console.log("------------");
+
+const pazymiai = [4, 6, 7, 5, 8, 9];
+console.log(pazymiai);
+
+const abc = ["d", "f", "h", "j", "k", "t"];
+console.log(abc);
+
+const pirmasPazymys = pazymiai[0];
+console.log(pirmasPazymys);
+
+const antrasPazymys = pazymiai[1];
+console.log(antrasPazymys);
+
+const ketvirtasPazymys = pazymiai[4];
+console.log(ketvirtasPazymys);
+
+const pazymiuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4] + pazymiai[5];
+console.log("Pazymiu suma:", pazymiuSuma);
+
+const pazymiuKiekis = pazymiai.length;
+const pazymiuVidurkis = pazymiuSuma / pazymiuKiekis;
+console.log("Pazymiu suma:", pazymiuSuma);
+console.log("Pazymiu kiekis:", pazymiuKiekis);
+console.log("Pazymiu vidurkis:", pazymiuVidurkis);
+
+console.log("------------");
+
+
+// PASKAITA 03.10
+
+const x = "sfsv.,";
+console.log(x);
+
+console.log("------------");
+
+const ab= ["a", "b", "c", "d", "e", "f"];
+console.log(ab);
+
+const abilgis = ab.length;
+const paskutinesRaidesPozicija = abilgis - 1;
---
+++ js/02-strings.js
@@ -0,0 +1,10 @@
+// VISOS KOMBINACIJOS DARBUI SU TEKSTU
+
+const menuo = "Kovo";
+const diena = "desimta";
+
+const dayString = "Siandien yra: " + menuo + " " + diena + ".";
+console.log(dayString);
+
+console.log("---------");
+const dvigubaKabute = "Cia yra dviguba (') dvigubaKabute.";
