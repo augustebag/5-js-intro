9eaec4d63ca9098175f4ab76a1a9e13c62e49a03
Auguste <abagdzeviciute@gmail.com>
2021-03-13 12:40:04 +0200

    if2

--- js/03-if.js
+++ js/03-if.js
@@ -35,3 +35,85 @@ if (g < h) {
     const ghNEmaziau = `${g} yra NE maziau uz ${h}`;
     console.log(ghNEmaziau);
 }
+
+console.log('---------------------');
+
+const j = 3;
+const k = 9;
+
+if ( j < k) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+const l = 66;
+const m = 29;
+
+if ( l != m) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+console.log('---------------------');
+
+const cookiesAgreed = false;
+
+if (cookiesAgreed === false) {
+    console.log('Ismetame cookies sutikimo bloka 1');
+}
+
+if (!cookiesAgreed) {
+    console.log('Ismetame cookies sutikimo bloka 2');
+}
+console.log('---------------------');
+
+const userLoggedIn = false;
+
+if (userLoggedIn) {
+    console.log('Log out')
+} else {
+    console.log('Log in');
+}
+
+console.log('---------------------');
+
+const pomidoras = 'pomidoras';
+const pomidoroIlgis = pomidoras.length;
+
+console.log(pomidoroIlgis);
+
+console.log('---------------------');
+ 
+const akys = 'labas'
+
+if (akys === 'Melynos') {
+    console.log('Kokie faini');
+} else if (akys === 'Zalios') {
+    console.log('Protingi');
+} else if (akys === 'Rudos') {
+    console.log('Melagis');
+} else {
+    console.log('Ar turi akis?');
+}
+
+console.log('---------------------');
+
+if (akys === 'Melynos') {
+    console.log('Kokie faini');
+}
+
+if (akys === 'Zalios') {
+    console.log('Protingi');
+}
+
+if (akys === 'Rudos') {
+    console.log('Melagiai');
+}
+
+if (akys !== 'Melynos' &&
+    akys !== 'Zalios' &&
+    akys !== 'Rudos') {
+        console.log('Ar tu turi akis?');
+    }
