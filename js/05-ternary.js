f71e942cbb6ab7d012bc162007230ea1fd08ba0c
Auguste <abagdzeviciute@gmail.com>
2021-03-15 20:31:13 +0200

    Ternary

---
+++ js/05-ternary.js
@@ -0,0 +1,20 @@
+ // TERNARY OPERATOR
+
+ const a = 9;
+ const b = 6;
+
+ let ats = '';
+
+ if (a > b) {
+     ats = 'daugiau';
+ } else {
+     ats = 'ne daugiau';
+ }
+
+ console.log(ats);
+
+ const c = 8;
+ const d = 6;
+ const ats2 = c > d ? 'daugiau' : 'ne daugiau';
+
+ console.log(ats2);
