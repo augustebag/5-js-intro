8723bf0a42f53684a6f98883f0fade9ca1a269bd
Auguste <abagdzeviciute@gmail.com>
2021-03-16 19:25:56 +0200

    Reverse

--- js/08-sum.js
+++ js/08-sum.js
@@ -12,6 +12,18 @@ Intervale nuo 0 iki 4 suma yra 10.
 
 */
 
+const nuo1 = 10;
+const iki1 = 14;
+let suma1 = 0;
+
+for (let i = nuo1; i <= iki1; i++) {
+    suma1 += i;
+}
+const ats1 = `Intervale nuo ${nuo1} iki ${iki1} suma yra ${suma1}.`;
+console.log(ats1);
+
+console.log('------------');
+
 const nuo = 0;
 const iki = 4;
 let suma = 0;
@@ -25,3 +37,22 @@ for (let i = 0; i <= iki; i++) {
  const ats = `Intervale nuo ${nuo}  iki ${iki} suma yra ${suma}.`;
 
 console.log(ats);
+
+console.log('---------');
+
+const nuo2 = 3;
+const iki2 = 10;
+let suma2 = 0;
+
+if (nuo2 <= iki2) {
+    for (let i = nuo2; i <= iki2; i++) {
+        suma2 += i;
+    }
+} else {
+    for (let i = iki2; i <= nuo2; i++) {
+        suma2 += i;
+    }
+}
+
+const ats2 = `Intervale nuo ${nuo2}  iki ${iki2} suma yra ${suma2}.`;
+console.log(ats2);
