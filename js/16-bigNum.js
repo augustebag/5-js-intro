5d6a5e2819b2e63bf6e7fb72b0a1f44fe1d79b11
Auguste <abagdzeviciute@gmail.com>
2021-03-23 21:41:54 +0200

    Didziausias skaicius, kai sarase gali buti bet kas

--- js/16-bigNum.js
+++ js/16-bigNum.js
@@ -9,14 +9,27 @@ function bigNum(list) {
     }
 
     // logic
-    let biggest = list[0];
+    let biggest = -Infinity;
 
     for (let i=0; i<list.length; i++) {
         const number = list[i];
+
+    // ar tai normalus skaicius
+        if (typeof number !== 'number' || !isFinite(number)) {
+            continue;                // eina i sekancia ciklo iteracija
+        }
+
+    // ar jis didesnis uz jau zinoma didziausia skaiciu
+
         if (number > biggest) {
             biggest = number;
         }
     }
+
+    // logic validation
+    if (biggest === -Infinity)
+        return 'ERROR: sarase nerastas nei vienas normalus skaicius';
+        
     // result
     return biggest;
 }
@@ -30,3 +43,6 @@ console.log(bigNum([1, 2, 3]), '->', 3);
 console.log(bigNum([-5, 78, 14, 0, 18]), '->', 78);
 console.log(bigNum([69, 69, 69, 69, 66]), '->', 69);
 console.log(bigNum([-1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
+console.log(bigNum([1, 8, 3, 5, Infinity]), '->', 8);
+console.log(bigNum([1, 8, Infinity, 3, 5]), '->', 8);
+console.log(bigNum([Infinity, 1, 8, 3, 5]), '->', 8);
